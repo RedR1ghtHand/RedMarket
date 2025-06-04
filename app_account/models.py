81c397ca6bb776c7eef531f8a7dd6b5f41243811
RedR1ghtHand <4aikaman@gmail.com>
2025-06-04 23:53:10 +0300

    feature(app_account) User model: migration - add new created_at field

--- app_account/models.py
+++ app_account/models.py
@@ -23,6 +23,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=30, unique=True)
     mc_username = models.CharField(max_length=30, blank=True, null=True, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
